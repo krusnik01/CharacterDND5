1c8c528f8a83acb48bfd9096210293012660a475
MyBook <g.o.d.00@mail.ru>
2020-08-16 19:51:30 +0300

    логика сортировки

--- app/src/main/java/com/demo/newcharacter/AddCharacterActivity.java
+++ app/src/main/java/com/demo/newcharacter/AddCharacterActivity.java
@@ -6,12 +6,16 @@ import androidx.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class AddCharacterActivity extends AppCompatActivity {
 
@@ -45,8 +49,13 @@ public class AddCharacterActivity extends AppCompatActivity {
     private ImageButton imageButtonCharUp;
     private ImageButton imageButtonCharDown;
 
+    private Button buttonRoll;
+
     private int idButton;
     private int idImageButton;
+    private int rollSize = 4;
+    private int minRandom =1;
+    private int maxRandom =6;
 
     private int strValue;
     private int dexValue;
@@ -54,6 +63,9 @@ public class AddCharacterActivity extends AppCompatActivity {
     private int intValue;
     private int wisValue;
     private int charValue;
+
+    private int freeValue;
+    private int minValue = 5;
 
     private MainViewModel viewModel;
 
@@ -64,8 +76,8 @@ public class AddCharacterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_character);
         viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         searchItem();
-        setAttributes();
-
+        getAttributes();
+        changesButtonPlus();
     }
 
     public void onClickCreateCharacter(View view) {
@@ -111,9 +123,19 @@ public class AddCharacterActivity extends AppCompatActivity {
                 hideFreeValue();
             }
             if ((idButton == R.id.radioButtonCustom) || (idButton == R.id.radioButtonPointBuy)) {
+                setDefaultPointBuy();
                 changesButtonPlus();
             } else {
                 changesButtonUp();
+            }
+            if (idButton == R.id.radioButtonSimple) {
+                setDefaultSimple();
+            }
+            if (idButton == R.id.radioButtonRoll) {
+                setDefaultRandom();
+                buttonRoll.setVisibility(View.VISIBLE);
+            } else {
+                buttonRoll.setVisibility(View.GONE);
             }
         }
     };
@@ -180,7 +202,6 @@ public class AddCharacterActivity extends AppCompatActivity {
         textViewWisdomValue = findViewById(R.id.textViewWisdomValue);
         textViewCharismaValue = findViewById(R.id.textViewCharismaValue);
 
-
         radioButtonCustom.setOnClickListener(radioButtonClickListener);
         radioButtonSimple.setOnClickListener(radioButtonClickListener);
         radioButtonPointBuy.setOnClickListener(radioButtonClickListener);
@@ -199,50 +220,178 @@ public class AddCharacterActivity extends AppCompatActivity {
         imageButtonCharUp.setOnClickListener(setAttributesClickListner);
         imageButtonCharDown.setOnClickListener(setAttributesClickListner);
 
+        buttonRoll = findViewById(R.id.buttonRoll);
     }
 
-    public void setAttributes() {
+    public void getAttributes() {
         strValue = Integer.parseInt(textViewStrengthValue.getText().toString());
         dexValue = Integer.parseInt(textViewDexterityValue.getText().toString());
-       conValue = Integer.parseInt(textViewConstitutionValue.getText().toString());
+        conValue = Integer.parseInt(textViewConstitutionValue.getText().toString());
         intValue = Integer.parseInt(textViewIntelligenceValue.getText().toString());
         wisValue = Integer.parseInt(textViewWisdomValue.getText().toString());
         charValue = Integer.parseInt(textViewCharismaValue.getText().toString());
+        freeValue = Integer.parseInt(textViewFreeValue.getText().toString());
+    }
 
+    public void setAttributes() {
+        textViewFreeValue.setText(String.format("%s", freeValue));
+        textViewStrengthValue.setText(String.format("%s", strValue));
+        textViewDexterityValue.setText(String.format("%s", dexValue));
+        textViewConstitutionValue.setText(String.format("%s", conValue));
+        textViewIntelligenceValue.setText(String.format("%s", intValue));
+        textViewWisdomValue.setText(String.format("%s", wisValue));
+        textViewCharismaValue.setText(String.format("%s", charValue));
     }
 
     View.OnClickListener setAttributesClickListner = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-           ImageButton imageButtonClick = (ImageButton) view;
+            ImageButton imageButtonClick = (ImageButton) view;
             idImageButton = imageButtonClick.getId();
-            setValueAttributes(idImageButton);
-            setAttributes();
+            if ((idButton == R.id.radioButtonPointBuy) && (freeValue > 0)) {
+                setValueAttributes(idImageButton);
+            }
+            if (idButton == R.id.radioButtonCustom) {
+                setValueAttributes(idImageButton);
+            }
+            if (idButton == R.id.radioButtonSimple) {
+                changeAttributes(imageButtonClick,idImageButton);
+            }
+            if (idButton == R.id.radioButtonRoll) {
+                changeAttributes(imageButtonClick,idImageButton);
+            }
+
         }
     };
 
-    private void setValueAttributes (int idImageButton){
+    private void setValueAttributes(int idImageButton) {
         switch (idImageButton) {
-            case R.id.imageButtonStrUp: strValue += 1; textViewStrengthValue.setText(String.format("%s", strValue)); break;
-            case R.id.imageButtonStrDown: strValue -= 1;  textViewStrengthValue.setText(String.format("%s", strValue)); break;
+            case R.id.imageButtonStrUp:
+                strValue = addAttributes(strValue);
+                break;
+            case R.id.imageButtonStrDown:
+                strValue = removeAttributes(strValue);
+                break;
 
-            case R.id.imageButtonDexUp: dexValue += 1; textViewDexterityValue.setText(String.format("%s", dexValue)); break;
-            case R.id.imageButtonDexDown: dexValue -= 1; textViewDexterityValue.setText(String.format("%s", dexValue)); break;
+            case R.id.imageButtonDexUp:
+                dexValue = addAttributes(dexValue);
+                break;
+            case R.id.imageButtonDexDown:
+                dexValue = removeAttributes(dexValue);
+                break;
 
-            case R.id.imageButtonConUp: conValue += 1; textViewConstitutionValue.setText(String.format("%s", conValue+ 1)); break;
-            case R.id.imageButtonConDown:conValue -= 1;  textViewConstitutionValue.setText(String.format("%s", conValue)); break;
+            case R.id.imageButtonConUp:
+                conValue = addAttributes(conValue);
+                break;
+            case R.id.imageButtonConDown:
+                conValue = removeAttributes(conValue);
+                break;
 
-            case R.id.imageButtonIntUp:  intValue += 1;textViewIntelligenceValue.setText(String.format("%s", intValue + 1)); break;
-            case R.id.imageButtonIntDown:  intValue -= 1;textViewIntelligenceValue.setText(String.format("%s", intValue)); break;
+            case R.id.imageButtonIntUp:
+                intValue = addAttributes(intValue);
+                break;
+            case R.id.imageButtonIntDown:
+                intValue = removeAttributes(intValue);
+                break;
 
-            case R.id.imageButtonWisUp:  wisValue += 1;textViewWisdomValue.setText(String.format("%s", wisValue+ 1)); break;
-            case R.id.imageButtonWisDown: wisValue -= 1; textViewWisdomValue.setText(String.format("%s", wisValue)); break;
+            case R.id.imageButtonWisUp:
+                wisValue = addAttributes(wisValue);
+                break;
+            case R.id.imageButtonWisDown:
+                wisValue = removeAttributes(wisValue);
+                break;
 
-            case R.id.imageButtonCharUp:  charValue += 1;textViewCharismaValue.setText(String.format("%s", charValue)); break;
-            case R.id.imageButtonCharDown:  charValue -= 1;textViewCharismaValue.setText(String.format("%s", charValue)); break;
-
+            case R.id.imageButtonCharUp:
+                charValue = addAttributes(charValue);
+                break;
+            case R.id.imageButtonCharDown:
+                charValue = removeAttributes(charValue);
+                break;
         }
+
+        setAttributes();
     }
 
+    public int addAttributes(int valueAttributes) {
+        if (freeValue > 0) {
+            valueAttributes += 1;
+            freeValue -= 1;
+        }
+        return valueAttributes;
+    }
 
+    public int removeAttributes(int valueAttributes) {
+        if (valueAttributes > minValue) {
+            valueAttributes -= 1;
+            freeValue += 1;
+        }
+        return valueAttributes;
+    }
+
+    public void setDefaultPointBuy() {
+        strValue = 8;
+        dexValue = 8;
+        conValue = 8;
+        intValue = 8;
+        wisValue = 8;
+        charValue = 8;
+        freeValue = 28;
+        setAttributes();
+    }
+
+    public void setDefaultRandom() {                                        //генерация ролл 4d6
+        strValue = setGenerateRandom();
+        dexValue = setGenerateRandom();
+        conValue = setGenerateRandom();
+        intValue = setGenerateRandom();
+        wisValue = setGenerateRandom();
+        charValue = setGenerateRandom();
+        setAttributes();
+    }
+    public int setGenerateRandom(){
+        int[] roll = new int[rollSize];
+        int minRoll = 10;
+        int minRollIndex = 0;
+        int summRoll = 0;
+        for (int i = 0; i < roll.length; i++) {
+            roll[i] = generateRandom();
+            if (minRoll > roll[i]) {
+                minRoll = roll[i];
+                minRollIndex = i;
+            }
+        }
+        roll[minRollIndex] = 0;
+        for (int i = 0; i < roll.length; i++) {
+            summRoll = summRoll + roll[i];
+        }
+        return summRoll;
+            }
+
+    public int generateRandom() {
+        int getRandom;
+        getRandom= minRandom+(int)(Math.random()*maxRandom);
+        return getRandom;
+    }
+
+    public void changeAttributes(ImageButton imageButtonClick, int idImageButton) {                // логика сортировки
+        String sosed = imageButtonClick.getContentDescription().toString();
+        //String idstring = textViewStrengthValue;
+       // Toast.makeText(this, ""+idstring, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    public void setDefaultSimple() {
+        strValue = 15;
+        dexValue = 14;
+        conValue = 13;
+        intValue = 12;
+        wisValue = 10;
+        charValue = 8;
+        setAttributes();
+    }
+
+    public void onClickGenerateRoll(View view) {
+        setDefaultRandom();
+    }
 }
